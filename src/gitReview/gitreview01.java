29f72099e2ce17b7c5872864e29dbc3451b5bb77
oksanash5 <oksanash55@gmail.com>
2022-05-31 21:52:41 -0400

    2nd commit

--- src/gitReview/gitreview01.java
+++ src/gitReview/gitreview01.java
@@ -4,7 +4,10 @@ public class gitreview01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+	     System.out.println("today we learn github");
+	     System.out.println("today is a very good day");
+	    System.out.println("today is a very good day");
 
-	}
-
+}
 }
